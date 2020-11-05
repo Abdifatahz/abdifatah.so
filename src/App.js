88dbfffdbeb10e239cda95d9f377014e48650b21
abdifatahz <faahid44@hotmail.com>
2020-11-05 13:26:30 +0300

    Added footer.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "./assets/main.css";
 import Home from "./components/home";
 
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App bg-blue-100 h-64">
       <Home />
+      <p className="mt-2 text-blue-700 text-center font-bold">
+        Build With React and Tailwing by Abdifatah Abdilahi
+      </p>
     </div>
   );
 }
