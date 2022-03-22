b3430d8865e443b7565c880c59ffc9e67163ce30
Abdifatahz <faahid44@gmail.com>
2022-03-22 16:20:31 +0300

    Changed ASCII icons to react-icons.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,6 +3,9 @@ import { Link } from "@reach/router";
 import { ThemeContext } from '../context/ThemeContextProvider';
 import { TogglerContext } from "../context/TogglerContextProvider";
 import MenuLink from './MenuLink';
+import {BsSunFill} from 'react-icons/bs'
+import {MdDarkMode} from 'react-icons/md'
+
 
 function Nav() {
   
@@ -33,29 +36,17 @@ function Nav() {
           </button>
         </div>
         <div className="w-full lg:flex lg:items-center lg:w-auto lg:justify-items-end">
-          <div
-            id="nav"
-            className={`text-sm lg:flex-grow ${
-              toggle ? "hidden md:block flex flex-col" : "sm:block"
-            }`}
-          >
-
+          <div id="nav" className={`text-sm lg:flex-grow ${toggle ? "hidden md:block flex flex-col" : "sm:block"}`}>
             <MenuLink to="/" >Home</MenuLink>
             <MenuLink to="/resume">Resume</MenuLink>
             <MenuLink to="/contact">Contact</MenuLink>
-
           </div>
-          <div
-            className={`${
-              toggle ? "hidden md:block flex flex-col" : "sm:block"
-            }`}
-          >
+          <div className={`${toggle ? "hidden md:block flex flex-col mt-3" : "sm:block"}`}>
             <Link
-              className="inline-block text-2xl px-4 py-2 transition  duration-700 ease-in-out text-white  mt-4 lg:mt-0 mode"
-              to="#"
-              onClick={changeMode}
-            >
-              {darkMode ? "🌞" : "🌙"}
+                className="inline-block text-2xl px-4 py-2 transition  duration-700 ease-in-out text-white lg:mt-0 mode"
+                to="#" onClick={changeMode}
+              >
+              {darkMode ? <BsSunFill/> : <MdDarkMode />}
             </Link>
           </div>
         </div>
